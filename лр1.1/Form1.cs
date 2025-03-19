748c096502986e9c983ccae695223f206bf09f93
ZMert <freyrab123@gmail.com>
2025-03-19 17:04:44 +0300

    Add files via upload

---
+++ лр1.1/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace лр1._1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = String.Empty;
+            Random r = new Random();
+            int id = 0;
+            for (int i = 0; i < 20; i++)
+            {
+               string p = "", d = "";
+                for (int l = 0; l < 4; l++)
+                {
+                    int a = r.Next(1, 3);
+                    if (a == 1) id = r.Next(97, 123);
+                    if (a == 2)
+                    {
+                        id = r.Next(1025, 1072);
+                        while ((id > 1025) & (id < 1040))
+                            id = r.Next(1025, 1072);
+                    }                        
+                    char s = (char)id;
+                    p = Convert.ToString(s);
+                    d += p;
+                }
+                textBox1.Text += d + Environment.NewLine;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = String.Empty;
+            Random r = new Random();
+            int id = 0;
+            for (int i = 0; i < 20; i++)
+            {
+                string p = "", d = "";
+                for (int l = 0; l < 7; l++)
+                {
+                    int a = r.Next(1, 3);
+                    if (a == 1) id = r.Next(33, 43);
+                    if (a == 2)
+                    {
+                        id = r.Next(1072, 1106);
+                        while (id == 1104)
+                            id = r.Next(1072, 1106);
+                    }
+                    char s = (char)id;
+                    p = Convert.ToString(s);
+                    d += p;
+                }
+                textBox1.Text += d + Environment.NewLine;
+            }
+        }
+    }
+}
---
+++ лр1.1/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("лр1.1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("лр1.1")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("5dffbd3a-fe07-4149-a2e2-33abd153d4f6")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
